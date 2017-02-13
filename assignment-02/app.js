8d8803256804c9c0792e689b333303897dff9e9f
Kang Chen <kangchen.us@gmail.com>
2017-02-13 04:55:56 -0800

    index.html update

--- assignment-02/app.js
+++ assignment-02/app.js
@@ -1,75 +1,83 @@
 (function () {
-    'use strict';
+    'use strict'
 
     var myApp = angular.module('myApp', [])
         .controller('ShoppingListController', ShoppingListController)
         .controller('CompletedListController', CompletedListController)
-        .service('ShoppingListService', ShoppingListService);
+        .service('ShoppingListService', ShoppingListService)
 
-    ShoppingListController.$inject = ['ShoppingListService'];
-
+    ShoppingListController.$inject = ['ShoppingListService']
     function ShoppingListController(ShoppingListService) {
 
-        var list1 = this;
+        var list1 = this
 
-        ShoppingListService.addItem('cookies', '10 pieces');
-        ShoppingListService.addItem('chips', '3 bags');
-        ShoppingListService.addItem('waters', '3 bottles');
-        ShoppingListService.addItem('fruits', '2 bags');
-        ShoppingListService.addItem('buffalo wings', '3 pounds');
+        ShoppingListService.addItem('cookies', '10 pieces')
+        ShoppingListService.addItem('chips', '3 bags')
+        ShoppingListService.addItem('waters', '3 bottles')
+        ShoppingListService.addItem('fruits', '2 bags')
+        ShoppingListService.addItem('buffalo wings', '3 pounds')
 
-        list1.items = ShoppingListService.getItemLists();
-        list1.name = '';
-        list1.quantity = '';
+        list1.items = ShoppingListService.getItemLists()
+        list1.name = ''
+        list1.quantity = ''
 
         list1.addItem = function () {
             if (list1.name === '' || list1.quantity === '') {} else {
-                ShoppingListService.addItem(list1.name, list1.quantity);
-                list1.name = '';
-                list1.quantity = '';
+                ShoppingListService.addItem(list1.name, list1.quantity)
+                list1.name = ''
+                list1.quantity = ''
             }
-        };
+        }
 
         list1.removeItem = function (idx) {
-            ShoppingListService.removeItem(idx);
-        };
+            ShoppingListService.removeItem(idx)
+        }
     }
 
 
-    CompletedListController.$inject = ['ShoppingListService'];
-
+    CompletedListController.$inject = ['ShoppingListService']
     function CompletedListController(ShoppingListService) {
 
-        var list2 = this;
-        list2.items = ShoppingListService.getDoneLists();
+        var list2 = this
+        list2.items = ShoppingListService.getDoneLists()
 
+        list2.removeItem = function (idx) {
+            ShoppingListService.removeDoneLists(idx)
+        }
     }
 
+
+// Shopping List Service
+
     function ShoppingListService() {
-        var service = this;
-        var itemLists = [];
-        var doneLists = [];
+        var service = this
+        var itemLists = []
+        var doneLists = []
 
         service.addItem = function (sName, iQuantity) {
             var item = {
                 name: sName,
                 quantity: iQuantity
-            };
-            itemLists.push(item);
-        };
+            }
+            itemLists.push(item)
+        }
 
         service.removeItem = function (idx) {
-            doneLists.push(itemLists[idx]);
-            itemLists.splice(idx, 1);
-        };
+            doneLists.push(itemLists[idx])
+            itemLists.splice(idx, 1)
+        }
 
         service.getItemLists = function () {
-            return itemLists;
-        };
+            return itemLists
+        }
 
         service.getDoneLists = function () {
-            return doneLists;
-        };
+            return doneLists
+        }
+
+        service.removeDoneLists = function (idx){
+            doneLists.splice(idx, 1)
+        }
     }
 
-})();
+})()
