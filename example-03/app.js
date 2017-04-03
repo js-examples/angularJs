f98478bca0dfe32fb3bb8dafdc9528ffccc09ddc
Kang Chen <kangchen.us@gmail.com>
2017-04-02 21:49:55 -0700

    example-03 updated

---
+++ example-03/app.js
@@ -0,0 +1,69 @@
+(function() {
+
+    'use strict';
+    angular.module('myApp', [])
+        .controller('NarrowItDownController', NarrowItDownController)
+        .service('MenuSearchService', MenuSearchService)
+        .constant('ApiEndPoint', 'https://davids-restaurant.herokuapp.com/')
+        .directive('foundItems', FoundItems);
+
+
+    function FoundItems() {
+        return {
+            restrict: 'AE',
+            templateUrl: 'foundItems.html',
+            scope: {
+                found: '<',
+                onRemove: '&'
+            }
+        };
+    }
+
+    MenuSearchService.$inject = ['$http', 'ApiEndPoint'];
+
+    function MenuSearchService($http, ApiEndPoint) {
+        var service = this;
+
+        service.getMatchedMenuItems = function() {
+            var foundItems = $http({
+                method: 'GET',
+                url: (ApiEndPoint + "/menu_items.json")
+            });
+
+            return foundItems;
+        }
+    }
+
+
+    NarrowItDownController.$inject = ['MenuSearchService'];
+
+    function NarrowItDownController(MenuSearchService) {
+        var search = this;
+        var found = [];
+        search.desc = '';
+
+        search.getSearchList = function() {
+            var promise = MenuSearchService.getMatchedMenuItems();
+            promise.then(function(res) {
+                    found = [];
+                    var tempStr;
+                    if(search.desc!=='')
+                       for (var i = 0, length = res.data.menu_items.length; i < length; i++) {
+                           tempStr = (res.data.menu_items[i].description).split(' ');
+                           if (tempStr.indexOf(search.desc.toLowerCase()) >= 0) {
+                               found.push(res.data.menu_items[i]);
+                           }
+                       }
+                    search.menuItems = found;
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
+        }
+
+        search.removeItem = function(idx){
+           found.splice(idx, 1);
+        }
+    }
+
+})();
